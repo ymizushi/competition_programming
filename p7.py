9a3198d93f50f6af770db66248be983fb43e896c
ymizushi <mizushi@gmail.com>
2013-05-27 03:55:32 +0900

    solve p7.py(this algorithm is not efficient)

---
+++ p7.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n is 1:
+        return False
+    for i in range(2,n):
+        if n % i is 0:
+            return False
+    return True
+
+def nth_prime(th):
+    index = 0
+    n = 2
+    while True:
+        if(is_prime(n)):
+            index += 1
+            if(index == th):
+                return n
+        n += 1
+print nth_prime(5000)
