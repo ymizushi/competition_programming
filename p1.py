0dee5f16354bbeeef0f56194fd0f3426ec26196d
yuta.mizushima <yuta.mizushima@glpgs.com>
2013-05-31 19:09:49 +0900

    edit p1 code

--- p1.py
+++ p1.py
@@ -1,5 +1,6 @@
-num_list = []
-for x in range(1,1000):
-    if x % 3 == 0 or x % 5 == 0:
-        num_list.append(x)
+num_list = filter( lambda x:x % 3 == 0 or x % 5 == 0, range(1,1000))
+# for x in range(1,1000):
+#     if x % 3 == 0 or x % 5 == 0:
+#         num_list.append(x)
+
 print "result:", sum(num_list)
