427993490d6821a1ba333ccaa4668aece4487123
yuta_mizushima <yuta_mizushima@dwango.co.jp>
2013-07-10 21:34:54 +0900

    add sample

--- p1.py
+++
@@ -1,6 +0,0 @@
-num_list = filter( lambda x:x % 3 == 0 or x % 5 == 0, range(1,1000))
-# for x in range(1,1000):
-#     if x % 3 == 0 or x % 5 == 0:
-#         num_list.append(x)
-
-print "result:", sum(num_list)
