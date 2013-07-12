b6308786f1bc57b723adb762ce7254310080c50f
yuta_mizushima <yuta_mizushima@dwango.co.jp>
2013-07-12 12:48:49 +0900

    edit part-sum

--- dw/p1.clj
+++ dw/p1.clj
@@ -1,12 +1,11 @@
-(def n 20)
-(def a [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13])
-(def k 444)
+(defn part-sum? [a k]
+  (let [n (count a)]
+    (defn part-sum? [index sum]
+      (cond
+        (= index n) (= sum k)
+        (part-sum? (inc index) sum) true
+        (part-sum? (inc index) (+ sum (get a index))) true 
+        true false))
+    (part-sum? 0 0)))
 
-(defn part-sum? [index sum]
-  (cond
-    (= index n) (= sum k)
-    (part-sum? (inc index) sum) true
-    (part-sum? (inc index) (+ sum (get a index))) true 
-    true false))
-
-(print (part-sum? 0 0))
+(println (part-sum? [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13] 444))
