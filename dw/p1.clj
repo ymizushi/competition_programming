5cc28c5c0dbdd4a54804e5b5b7492f7960d7e691
ymizushi <mizushi@gmail.com>
2013-07-12 09:26:49 +0900

    edit p1.

--- dw/p1.clj
+++ dw/p1.clj
@@ -1,64 +1,17 @@
-; 整数a1, a2, ..., anが与えられます。その中からいくつか選び、その和を丁度kにすることができるか判定しなさい。
-; 
-; 制約 (iは任意の整数)
-; 
-; 1 ≦ n ≦ 20
-; -10^8 ≦ ai ≦ 10^8
-; -10^8 ≦ k ≦ 10^8
-
-; n=20
-; a = {1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13}
-; k=444
+(def n 20)
+(def a [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13])
+(def k 444)
 
 
+(defstruct num-map :sum-flg :searched)
 
-;# n=20
-;# a = [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13]
-;# k=444
-;# 
-;# a.sort();
-;# print a
-;# 
-;# a_sorted = [-500, -23, 1, 1, 3, 3, 7, 8, 8, 10, 10, 13, 13, 23, 42, 45, 49, 49, 60, 599]
-;# 
-;# print sum(filter(lambda x: x> 0, a))
-;# print sum(filter(lambda x: x<= 0, a))
-;# 
-;# 
-;# 
-;# 
-;# 
-;# 
-;# positive_hoge = [1, 1, 3, 3, 7, 8, 8, 10, 10, 13, 13, 23, 42, 45, 49, 49, 60, 599]
-;# 
-;# a = [1,2,4,7]
-;# k = 13
-;# 
-;# 
-;# 
-;# a = [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13]
-;# def nc2_pat(a):
-;#     for e in a:
-;#         pass
-;# 
-;
-;def select_ele(list):
-;    return (list.pop(0), list)
-;
-;def select_eleset(eleset):
-;    hoge = eleset[1]
-;    return_list = []
-;    while hoge:
-;        return_list.append((eleset[0],hoge.pop()))
-;    return (hoge, return_list)
-;
-;# print select_ele([1,2,3,4,5])
-;# 
-;# print select_eleset((5, [1, 2, 3, 4]))
-;
-;print select_eleset(select_ele([1,2, 3,4,5]))
-;
-;
-;
-;
-;
+(def sample [(struct num-map false false) (struct num-map false false) (struct num-map false false)])
+
+(defn sum-flg-list [num-map-list]
+  (let [head (pop num-map-list) booby (last head)]
+    (cond
+      (:searched booby)
+        (conj head (struct num-map true false))
+      (not (:searched booby))
+        (conj head )
+    )))
