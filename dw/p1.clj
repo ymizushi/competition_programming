ee42ca576b4b16974c20c59460aaefa9f3b2ae68
yuta_mizushima <yuta_mizushima@dwango.co.jp>
2013-07-12 12:49:57 +0900

    edit line

--- dw/p1.clj
+++ dw/p1.clj
@@ -7,5 +7,4 @@
         (part-sum? (inc index) (+ sum (get a index))) true 
         true false))
     (part-sum? 0 0)))
-
 (println (part-sum? [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13] 444))
