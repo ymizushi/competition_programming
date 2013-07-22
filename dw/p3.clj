18d03a92e65a9cd6410bfd66a20cd6d570f3992b
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2013-07-22 12:57:12 +0900

    edit p3

--- dw/p3.clj
+++ dw/p3.clj
@@ -4,8 +4,14 @@
 (def s '(1 2 4 6 8))
 (def t '(3 5 7 9 10))
 
+(defn zip [a b]
+  (for [i (range (count a))]
+     (list (nth a i) (nth b i))))
+
 (def set-all (set '(1 2 3 4 5 6 7 8 9)))
 (def list-subset (list (set '(1 2)) (set '(2 3 4)) (set '(4 5 6)) (set '(6 7 8)) (set '(8 9))))
+
+(println (zip [1 2 3 4] [2 3 4 5]))
 
 (defn min-numlist [l]
   (first (sort l)))
@@ -21,13 +27,11 @@
           (search (clojure.set/difference set-all (rest subset-list)) (inc count))
           count)
         count))))
-
-(def set-p (set '(1 2 3 4 5 6 7 8 9)))
-(def set-st (list (set '(1 2)) (set '(2 3 4)) (set '(4 5 6)) (set '(6 7 8)) (set '(8 9))))
-(defn min-count [sets]
-  (filter #([e] (> (count e) ))))
-
-(println (search list-subset set-all 0))
+;
+;(def set-p (set '(1 2 3 4 5 6 7 8 9)))
+;(def set-st (list (set '(1 2)) (set '(2 3 4)) (set '(4 5 6)) (set '(6 7 8)) (set '(8 9))))
+;
+;(println (search list-subset set-all 0))
 
 ;(println (clojure.set/subset? (nth list-subset 0) set-all))
 ;(println (nth list-subset 0) )
