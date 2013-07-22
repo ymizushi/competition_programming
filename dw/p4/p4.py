a4b1de5054d18279025b3bada547060ef1b979d7
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2013-07-22 12:18:29 +0900

    edit p4

--- dw/p4/p4.py
+++ dw/p4/p4.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ファイルからテストデータ読み込み
 def parse(filename):
     data = dict()
     with open(filename) as f:
@@ -5,45 +9,48 @@ def parse(filename):
             data_set = line.strip('\n').split(' ')
             data[data_set[0]] = data_set[1]
     return data
-
 test_data =  parse('test_data.txt')
-
 s = test_data.get('s')
 t = test_data.get('t')
 
+# メモ化のためのハッシュマップを用意
 memo_map = dict()
-
+def gen_key(x, y):
+    return str(x)+"_"+str(y)
 def get_memo(x, y):
-    key = str(x)+str(y)
+    key = gen_key(x, y)
     if memo_map.get(key):
         return memo_map[key]
     return None
-
 def set_memo(x, y, count):
-    key = str(x)+str(y)
+    key = gen_key(x, y)
     memo_map[key] = count
 
-# return integer
+# (x, y-1), (x-1, y), がわかれば(x ,y)が計算出来る
 def calc(x, y):
-    if x < 0 or y < 0 or len(s)-1 < x or len(t)-1 < y:
+    if x < 0 or y < 0 or len(s)-1 < x or len(t)-1 < y: # index外は 0を返す
         return 0
+
+    # s[index] == t[index]なら カウントを増やす
     up_count = 0
     if s[x] == t[y]:
         up_count +=1
 
+    # (x, y-1)についてメモ化されていれば取得、無ければ計算してメモ化
     calcx = get_memo(x, y-1)
     if not calcx:
         calcx = calc(x, y-1)
         set_memo(x,y-1,calcx)
 
+    # (x-1, y)についてメモ化されていれば取得、無ければ計算してメモ化
     calcy = get_memo(x-1, y)
     if not calcy:
         calcy = calc(x-1, y)
         set_memo(x-1, y, calcy)
 
-    nowcount = max(calcx, calcy) + up_count 
-    return nowcount
+    return max(calcx, calcy) + up_count
 
+# sの長さ * tの長さだけループ
 def __main__():
     max_count = 0
     for y in range(len(t)):
