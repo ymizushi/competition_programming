2f0f6e21da194df4c14fafc5197a0499b17196d9
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2013-07-23 12:54:44 +0900

    edit test data

--- dw/p4/test_p4.py
+++ dw/p4/test_p4.py
@@ -4,9 +4,11 @@ from p4 import P4
 class TestP4(unittest.TestCase):
     def setUp(self):
         self.p4 = P4('test_data.txt')
+        self.p4_test1 = P4('test_data1.txt')
 
     def test_solve(self):
-        self.assertEqual(self.p4.calc_all(), 467)
+        # self.assertEqual(self.p4.calc_all(), 467)
+        self.assertEqual(self.p4_test1.calc_all(), 3)
 
 if __name__ == '__main__':
     unittest.main()
