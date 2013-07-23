5b1048bf5fbf38e6ac80bf74cefae939cecf6da9
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2013-07-23 12:17:39 +0900

    dit code

--- dw/p3/p3.py
+++ dw/p3/p3.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# ファイルからテストデータ読み込み
-def parse(filename):
-    data = dict()
-    with open(filename) as f:
-        for line in f:
-            data_set = line.strip('\n').split(' ')
-            data[data_set[0]] = data_set[1].split(',')
-    return data
-test_data =  parse('test_data.txt')
-s = map(lambda x: int(x), test_data.get('s'))
-t = map(lambda x: int(x), test_data.get('t'))
+class GreedMethod():
+    # ファイルからテストデータ読み込み
+    def parse(self, filename):
+        data = dict()
+        with open(filename) as f:
+            for line in f:
+                data_set = line.strip('\n').split(' ')
+                data[data_set[0]] = data_set[1].split(',')
+        return data
 
-def exclude_list(sublist, all_list):
-    subset = set(sublist)
-    allset = set(all_list)
-    if subset.issubset(allset):
-        return list(allset.difference(subset))
-    else:
-        return None
+    def __init__(self,filename):
+        test_data =  self.parse(filename)
+        s = map(lambda x: int(x), test_data.get('s'))
+        t = map(lambda x: int(x), test_data.get('t'))
+        self.all_st = range(min(s + t),max(s+t))
+        self.t_s = sorted(map(lambda x: range(x[0], x[1]), zip(s, t)), key=lambda x:len(x))
 
-def exclude_list_list(sublist, all_list):
-    return filter(lambda x: x != sublist, all_list)
-
-all_st = range(min(s + t),max(s+t))
-
-t_s = sorted(map(lambda x: range(x[0], x[1]), zip(s, t)), key=lambda x:len(x))
-
-def calc(sublist, alllist, count):
-    for sub in sublist:
-        if set(sub).issubset(set(alllist)):
-            return calc(exclude_list_list(sub, sublist), exclude_list(sub, alllist), count+1)
+    def exclude_list(self,sublist, all_list):
+        subset = set(sublist)
+        allset = set(all_list)
+        if subset.issubset(allset):
+            return list(allset.difference(subset))
         else:
-            continue
-    return count
+            return None
 
-print calc(t_s, all_st, 0)
+    def exclude_list_list(self,sublist, all_list):
+        return filter(lambda x: x != sublist, all_list)
 
 
-class Hoge:
-    pass
+    def calc(self,sublist, alllist, count):
+        for sub in sublist:
+            if set(sub).issubset(set(alllist)):
+                return self.calc(self.exclude_list_list(sub, sublist), self.exclude_list(sub, alllist), count+1)
+            else:
+                continue
+        return count
--- dw/p3/test_p3.py
+++ dw/p3/test_p3.py
@@ -1,16 +1,12 @@
 import unittest
-from p3 import *
+from p3 import GreedMethod
 
 class TestP3(unittest.TestCase):
     def setUp(self):
-        self.test_data =  parse('test_data.txt')
-        self.s = map(lambda x: int(x), test_data.get('s'))
-        self.t = map(lambda x: int(x), test_data.get('t'))
-        self.all_st = range(min(s + t),max(s+t))
-        self.t_s = sorted(map(lambda x: range(x[0], x[1]), zip(s, t)), key=lambda x:len(x))
+        self.greed_method = GreedMethod('test_data.txt')
 
     def test_solve(self):
-        self.assertEqual(calc(self.t_s, all_st, 0),10)
+        self.assertEqual(self.greed_method.calc(self.greed_method.t_s, self.greed_method.all_st, 0), 10)
 
 if __name__ == '__main__':
     unittest.main()
