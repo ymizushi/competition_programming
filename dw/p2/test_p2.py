97bd87190de2dec297ecd5e8927033247bc9205a
ymizushi <mizushi@gmail.com>
2013-07-25 04:58:10 +0900

    editest

--- dw/p2/test_p2.py
+++ dw/p2/test_p2.py
@@ -6,9 +6,10 @@ class TestP2(unittest.TestCase):
         pass
 
     def test_solve(self):
-        self.p2 = P2('test_data.txt')
-        self.assertEqual(self.p2.calc(), 22)
-        pass
+        # self.p2 = P2('test_data.txt')
+        self.p2_test1 = P2('test_data1.txt')
+        # self.assertEqual(self.p2.calc(), 22)
+        self.assertEqual(self.p2_test1.calc(), 22)
 
 if __name__ == '__main__':
     unittest.main()
