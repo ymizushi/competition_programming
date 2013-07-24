9406fb9fa64559e9c4f947081de4f55b4f3e6c40
ymizushi <mizushi@gmail.com>
2013-07-25 02:48:56 +0900

    add test code

--- dw/p1/test/p1/core_test.clj
+++ dw/p1/test/p1/core_test.clj
@@ -1,10 +1,8 @@
 (ns p1.core-test
   (:require [clojure.test :refer :all]
-            [p1.core :refer :all]))
+            [p1.core :refer :all]
+            [p1.data :refer [a k]]))
 
 (deftest test-part_sum? []
   (testing "core/part-sum?"
-    (let
-      [a [1 10 49 3 8 13 7 23 60 -500 42 599 45 -23 1 10 49 3  8 13]
-       k 444]
-      (is (= true (part-sum? [1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8,13] 444)))))) 
+    (is (= true (part-sum? a k))))) 
