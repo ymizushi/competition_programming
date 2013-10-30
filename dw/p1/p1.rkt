5f156437532a99c60000124aab6eb42cd9c24907
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2013-10-30 21:15:32 +0900

    add p1.rkt

---
+++ dw/p1/p1.rkt
@@ -0,0 +1,12 @@
+#!/usr/bin/env racket
+#lang racket
+(define (part-sum? a k)
+  (let ([n (length a)])
+    (define (part-sum? index sum)
+      (cond
+        [(= index n) (= sum k)]
+        [(part-sum? (+ index 1) sum) true]
+        [(part-sum? (+ index 1) (+ sum (list-ref a index))) true] 
+        [true false]))
+    (part-sum? 0 0)))
+(part-sum? '(1 10 49 3 8 13 7 23 60 -500 42 599 45 -23 1 10 49 3 8 13) 444)
