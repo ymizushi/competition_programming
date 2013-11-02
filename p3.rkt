ac9ea11a57ec8a0fc4a4e081e6f80b3632ffcde0
ymizushi <mizushi@gmail.com>
2013-11-02 16:55:26 +0900

    edit p3.rkt

--- p3.rkt
+++ p3.rkt
@@ -3,7 +3,13 @@
 
 (define (prime? n)
   (cond 
-    [(< n 3) false]
-    [(>= n 3) 
-      (filter (lambda (x) (/ x n) (range n)))
-     ]))
+    [(= n 1) false]
+    [(= n 2) true ]
+    [(= n 2) true ]
+    [else 
+      (let loop ([cnt 2])
+        (if (= (% n cnt) 0)
+          false
+          (if (<= (int (cnt math->sqrt)))
+            false
+            (loop (+ cnt 1)))))]))
