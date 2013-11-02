d24ac5401c5a3eb9d0f1d8fa72bf4d3d666cb51a
ymizushi <mizushi@gmail.com>
2013-11-02 14:22:52 +0900

    edit p2.clj

--- p2.clj
+++ p2.clj
@@ -1,10 +1,10 @@
 #!/usr/bin/env clj
-;(defn fib-slow [n]
-;  (cond
-;    (= n 0) 1
-;    (= n 1) 1
-;    true (bigint (+ (fib-slow (bigint (- n 2))) (fib-slow (bigint (- n 1)))))))
-;(println (time (fib-slow 40)))
+(defn fib-slow [n]
+  (cond
+    (= n 0) 1
+    (= n 1) 1
+    true (bigint (+ (fib-slow (bigint (- n 2))) (fib-slow (bigint (- n 1)))))))
+(println (time (fib-slow 40)))
 
 (defn fib [n]
   (defn fibl [n n-2 n-1]
