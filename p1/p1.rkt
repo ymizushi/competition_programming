12c6421d482d9d00e0f7829e3fb13dee2db44947
ymizushi <mizushi@gmail.com>
2013-11-03 18:37:16 +0900

    add p1.rkt

---
+++ p1/p1.rkt
@@ -0,0 +1,17 @@
+#!/usr/bin/env racket
+#lang racket
+(require test-engine/racket-tests)
+
+(define (sum-multi n [sum 0])
+  (if (= n 0)
+    sum
+    (if (or
+          (= 0 (remainder n 3))
+          (= 0 (remainder n 5)))
+      (sum-multi (- n 1) (+ sum n))
+      (sum-multi (- n 1) sum))))
+(sum-multi 999)
+
+(check-expect (sum-multi 9) 23)
+(check-expect (sum-multi 999) 233168)
+(test)
