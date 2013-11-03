fa9b810ef1a02c9ae207d295c15cf3e75668083c
ymizushi <mizushi@gmail.com>
2013-11-03 19:51:53 +0900

    edit clj

--- dw/p1/p1.clj
+++ dw/p1/p1.clj
@@ -1,4 +1,6 @@
-(ns p1.core)
+(use 'clojure.test)
+(def a [1 10 49 3 8 13 7 23 60 -500 42 599 45 -23 1 10 49 3 8 13])
+(def k 444)
 
 (defn part-sum? [a k]
   (let [n (count a)]
@@ -9,3 +11,8 @@
         (part-sum? (inc index) (+ sum (get a index))) true 
         true false))
     (part-sum? 0 0)))
+
+(deftest test-part_sum? []
+  (testing "core/part-sum?"
+    (is (= true (part-sum? a k))))) 
+(run-tests)
--- dw/p1/test/p1/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns p1.core-test
-  (:require [clojure.test :refer :all]
-            [p1.core :refer :all]
-            [p1.data :refer [a k]]))
-
-(deftest test-part_sum? []
-  (testing "core/part-sum?"
-    (is (= true (part-sum? a k))))) 
