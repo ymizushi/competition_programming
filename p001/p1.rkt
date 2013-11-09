d0c8544a3d88c578f8e1d2b9fd9e257c9cafa4b3
ymizushi <mizushi@gmail.com>
2013-11-10 03:58:53 +0900

    add README.md

--- p001/p1.rkt
+++ p001/p1.rkt
@@ -10,6 +10,17 @@
           (= 0 (remainder n 5)))
       (sum-multi (- n 1) (+ sum n))
       (sum-multi (- n 1) sum))))
+
+;dot表記を試してみた
+(define (sum-multi-dot n [sum 0])
+  (if (n . = . 0)
+    sum
+    (if ((0 . = . (n . remainder . 3))
+          . or .
+          (0 . = . (n . remainder . 5)))
+      (sum-multi-dot ( n . - . 1) (sum . + . n))
+      (sum-multi-dot ( n . - . 1) sum))))
+
 (sum-multi 999)
 
 (check-expect (sum-multi 9) 23)
--- p003/p3.rkt
+++ p003/p3.rkt
@@ -1,15 +1,32 @@
 #!/usr/bin/env racket
 #lang racket
+(require test-engine/racket-tests)
 
 (define (prime? n)
   (cond 
-    [(= n 1) false]
-    [(= n 2) true ]
-    [(= n 2) true ]
+    [(not (integer? n)) (begin (print "n must be integer!") false)]
+    [(< n 2) false]
     [else 
       (let loop ([cnt 2])
-        (if (= (% n cnt) 0)
-          false
-          (if (<= (int (cnt math->sqrt)))
-            false
-            (loop (+ cnt 1)))))]))
+        (if (= (remainder n cnt) 0)
+          (if (= n cnt) true false)
+          (loop (+ 1 cnt))))]))
+(check-expect (prime? 1) false)
+(check-expect (prime? 2) true)
+(check-expect (prime? 3) true)
+(check-expect (prime? 19) true)
+
+(define (list-prime n)
+  (let loop ([i 0] [prime-list null])
+    (if (= n i)
+      prime-list
+      (if (prime? i)
+        (loop (+ 1 i) (cons i prime-list))
+        (loop (+ 1 i) prime-list)))))
+
+(define (max-prime n)
+  )
+
+(max-prime 10)
+
+(test)
