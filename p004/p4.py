774b1280acc6e7647a4dacaf5aea64d045236147
ymizushi <mizushi@gmail.com>
2013-11-17 17:40:36 +0900

    add p4.rkt

--- p004/p4.py
+++ p004/p4.py
@@ -18,4 +18,3 @@ class TestMerge(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
---
+++ p004/p4.rkt
@@ -0,0 +1,25 @@
+#!/usr/bin/env racket
+#lang racket
+(require test-engine/racket-tests)
+
+
+(define (max-pal)
+  (let loop-n ([n 989])
+    (let loop-x ([x 999])
+      (list n x) (let* ([s-n (number->string n)]
+             [target (string->number (string-append
+                                 s-n
+                                 (list->string (reverse (string->list s-n)))
+                                 ))]
+             [y (/ target x)])
+        (if (and
+              (= 0 (remainder target x))
+              (= 3 (length (string->list (number->string y)))))
+          target
+          (cond
+            [(not (= x 100)) (loop-x (- x 1))]
+            [(not (= n 100)) (loop-n (- n 1))]
+            [else null]))))))
+
+(check-expect (max-pal) 906609)
+(test)
