eb694310772c3b57baf029f14f2929e06e2798c5
ymizushi <mizushi@gmail.com>
2013-11-17 22:11:50 +0900

    edit code

--- p007/p7.py
+++ p007/p7.py
@@ -1,7 +1,8 @@
+from math import sqrt
 def is_prime(n):
     if n is 1:
         return False
-    for i in range(2,n):
+    for i in range(2, int(sqrt(n))+1):
         if n % i is 0:
             return False
     return True
@@ -15,4 +16,4 @@ def nth_prime(th):
             if(index == th):
                 return n
         n += 1
-print nth_prime(5000)
+print nth_prime(10001)
