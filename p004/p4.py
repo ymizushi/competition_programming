b75469e39a5097738b29878abe618e7ce7d571fb
ymizushi <mizushi@gmail.com>
2013-11-17 15:09:12 +0900

    solve project euler p4

---
+++ p004/p4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+def max_palid():
+    for n in range(989, 100, -1):
+        for x in range(999, 100, -1):
+            target = int(str(n) + str(n)[::-1])
+            if (target % x == 0):
+                y = target / x
+                if (len(str(y)) == 3):
+                    return target
+
+class TestMerge(unittest.TestCase):
+    def test_solve(self):
+        expected = 906609
+        self.assertEqual(max_palid(), expected)
+
+if __name__ == '__main__':
+    unittest.main()
+
