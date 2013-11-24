3c363f6cb5335cf4ab745f18f48e38d916465717
ymizushi <mizushi@gmail.com>
2013-11-25 02:13:40 +0900

    edit rkt file

--- p003/p3.rkt
+++ p003/p3.rkt
@@ -6,27 +6,26 @@
   (cond 
     [(not (integer? n)) (begin (print "n must be integer!") false)]
     [(< n 2) false]
+    [(= n 2) true]
     [else 
-      (let loop ([cnt 2])
-        (if (= (remainder n cnt) 0)
-          (if (= n cnt) true false)
-          (loop (+ 1 cnt))))]))
+      (let loop ([cnt 3])
+        (if (= cnt (round (sqrt n)))
+          true
+          (if (= (remainder n cnt) 0)
+            false
+            (loop (add1 cnt)))))]))
 (check-expect (prime? 1) false)
 (check-expect (prime? 2) true)
 (check-expect (prime? 3) true)
 (check-expect (prime? 19) true)
 
-(define (list-prime n)
-  (let loop ([i 0] [prime-list null])
-    (if (= n i)
-      prime-list
-      (if (prime? i)
-        (loop (+ 1 i) (cons i prime-list))
-        (loop (+ 1 i) prime-list)))))
-
-(define (max-prime n)
-  )
-
-(max-prime 10)
-
+;(define (last-prime n)
+;  (let loop ([i (round (/ n 2))])
+;    (if (= n i)
+;      prime-list
+;      (if (prime? i)
+;        i
+;        (loop (add1 i))))))
+;
+;(check-expect (apply max (last-prime 600851475143)) 17)
 (test)
---
+++ p010/p10.rkt
@@ -0,0 +1,32 @@
+#!/usr/bin/env racket
+#lang racket
+(require test-engine/racket-tests)
+
+(define (prime? n)
+  (cond 
+    [(not (integer? n)) (begin (print "n must be integer!") false)]
+    [(< n 2) false]
+    [else 
+      (let loop ([cnt 2])
+        (if (= (remainder n cnt) 0)
+          (if (= n cnt) true false)
+          (loop (+ 1 cnt))))]))
+(check-expect (prime? 1) false)
+(check-expect (prime? 2) true)
+(check-expect (prime? 3) true)
+(check-expect (prime? 19) true)
+
+(define (list-prime n)
+  (let loop ([i 0] [prime-list null])
+    (if (= n i)
+      prime-list
+      (if (prime? i)
+        (loop (+ 1 i) (cons i prime-list))
+        (loop (+ 1 i) prime-list)))))
+
+(define (max-prime n))
+
+(max-prime 10)
+
+(test)
+
