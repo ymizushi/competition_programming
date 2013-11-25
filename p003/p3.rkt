a49b8ebff92667bdcac5137752fa548e753817e6
ymizushi <mizushi@gmail.com>
2013-11-26 01:52:51 +0900

    solve 03 by racket

--- p003/p3.rkt
+++ p003/p3.rkt
@@ -5,27 +5,28 @@
 (define (prime? n)
   (cond 
     [(not (integer? n)) (begin (print "n must be integer!") false)]
-    [(< n 2) false]
+    [(= n 1) false]
     [(= n 2) true]
     [else 
-      (let loop ([cnt 3])
-        (if (= cnt (round (sqrt n)))
-          true
+      (let loop ([cnt 2])
+        (if (<= cnt (round (sqrt n)))
           (if (= (remainder n cnt) 0)
             false
-            (loop (add1 cnt)))))]))
+            (loop (add1 cnt)))
+          true))]))
 (check-expect (prime? 1) false)
 (check-expect (prime? 2) true)
 (check-expect (prime? 3) true)
 (check-expect (prime? 19) true)
 
-;(define (last-prime n)
-;  (let loop ([i (round (/ n 2))])
-;    (if (= n i)
-;      prime-list
-;      (if (prime? i)
-;        i
-;        (loop (add1 i))))))
+(define (prime-lst n)
+  (let loop ([i 2] [n n] [l (list)])
+    (if (i . <= . n)
+      (if (and (= 0 (remainder n i)) (prime? i))
+        (let ([max (/ n i)])
+          (loop 2 max (append l (list i))))
+        (loop (add1 i) n l))
+      (apply max l))))
 ;
-;(check-expect (apply max (last-prime 600851475143)) 17)
+(check-expect (prime-lst 600851475143) 6857)
 (test)
