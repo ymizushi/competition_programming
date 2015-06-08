e7ea3b78af8e834906da80ee8d761ae3ad78f3eb
ymizushi <mizushi@gmail.com>
2015-06-08 15:59:09 +0900

    edit code

--- book/functional-scala/excercise2-1.scala
+++ book/functional-scala/excercise2-1.scala
@@ -1,4 +1,5 @@
 def fib(x: Int): Int = {
+  @annotation.tailrec
   def fib_n(n_2: Int, n_1:Int, n: Int):Int = {
     if (n == x) {
       n_2 + n_1
@@ -9,5 +10,16 @@ def fib(x: Int): Int = {
   fib_n(0,1,1)
 }
 
+// @annotation.tailrec
+def fib_b(n: Int): Int = {
+  if (n <= 0) {
+    0
+  } else if (n == 1) {
+    1
+  } else {
+    fib(n-2) + fib(n-1)
+  }
+}
 
 println(fib(4))
+println(fib_b(4))
