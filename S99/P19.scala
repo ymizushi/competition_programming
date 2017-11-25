24bdf90a462cb2b6487670ab904656210c591b49
ymizushi <mizushi@gmail.com>
2017-11-26 02:14:56 +0900

    add minus index to S99 P19

--- S99/P19.scala
+++ S99/P19.scala
@@ -1,11 +1,13 @@
 object P19 {
   def main(args: Array[String]) = {
     println(rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+    println(rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
   }
 
   def rotate[A](n: Int, l: List[A]): List[A] = {
     def innerRotate[A](count: Int, archived: List[A], rest: List[A]): List[A] = {
-      if (count == n) {
+      val index = if (count == n) n else l.length + n
+      if (count == index) {
         rest ++ archived
       } else {
         innerRotate(count+1, archived :+ rest.head, rest.tail)
