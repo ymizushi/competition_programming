877d08de2ba8741bb6b8ea415cf9c63f7308c84c
ymizushi <mizushi@gmail.com>
2017-11-26 01:53:24 +0900

    solve S99 P18

---
+++ S99/P18.scala
@@ -0,0 +1,19 @@
+object P18 {
+  def main(args: Array[String]) = {
+    println(slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  }
+
+
+  def slice[A](first: Int, last: Int, l: List[A]): List[A] = {
+    def innerSlice[A](count: Int, archived: List[A], rest: List[A]): List[A] = {
+      if (first <= count && count < last) {
+        innerSlice(count+1, archived :+ rest.head, rest.tail)
+      } else if (count == last) {
+        archived
+      } else {
+        innerSlice(count+1, archived, rest.tail)
+      }
+    }
+    innerSlice(0, List.empty, l)
+  }
+}
