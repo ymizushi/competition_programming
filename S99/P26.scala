65e04f99a98f61d7f0d22218293713fb3b8644e3
ymizushi <mizushi@gmail.com>
2017-11-28 23:26:05 +0900

    retry S99 P26

---
+++ S99/P26.scala
@@ -0,0 +1,11 @@
+object P26 {
+  def main(args: Array[String] = {
+      println(combinations(3, List('a, 'b, 'c, 'd, 'e, 'f)))
+  }
+
+  // RETRY
+  def combinations[A: Ordering](n: Int, l: List[A]): List[List[A]] = {
+      
+    }
+  }
+}
