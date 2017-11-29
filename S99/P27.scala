a738e42b221249b5992751a8d08441e5a73025fc
ymizushi <mizushi@gmail.com>
2017-11-29 23:29:50 +0900

    retry S99 P27

---
+++ S99/P27.scala
@@ -0,0 +1,10 @@
+object P27 {
+  def main(args: Array[String]) = {
+    println(List("Aldo", "Beat", "Carla", "David" , "Evi", "Flip", "Gary", "Hugo", "Gary", "Hugo", "Ida"))
+  }
+
+  // RETRY:
+  def group3[A](l: List[A]): List[List[List[A]]] = {
+
+  }
+}
