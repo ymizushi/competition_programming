c38ed24636b3acb2ea112007a46638487c7d563f
ymizushi <mizushi@gmail.com>
2017-11-30 22:11:39 +0900

    solve S99 P34

---
+++ S99/P34.scala
@@ -0,0 +1,29 @@
+object P34 {
+  implicit class AddIntTo(n: Int) {
+    def totient: Int = {
+      (1 to n).filter(isCoprimeTo(_)).length
+    }
+
+    def isCoprimeTo(m: Int): Boolean = {
+      n.gcd(m) == 1
+    }
+
+    def gcd(m: Int): Int = {
+      commonMultiple(n).filter { e =>
+        if (commonMultiple(m).contains(e)) {
+          true
+        } else {
+          false
+        }
+      }.last
+    }
+
+  def commonMultiple(n: Int): List[Int] = {
+    (1 to n).toList.filter(e => n % e  == 0)
+  }
+  }
+
+  def main(args: Array[String]) = {
+    println(10.totient)
+  }
+}
