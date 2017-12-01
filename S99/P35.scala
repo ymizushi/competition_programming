cc50c9384813181c4b03c346319c1a6faef732fd
ymizushi <mizushi@gmail.com>
2017-12-02 06:55:10 +0900

    solve S99 P35

---
+++ S99/P35.scala
@@ -0,0 +1,20 @@
+object P35 {
+  implicit class AddIntTo(n: Int) {
+    def primeFactors: List[Int] = {
+      def innerPrimeFactors(in: Int, divider: Int, archived: List[Int]): List[Int] = {
+        if (divider == n) {
+          archived
+        } else if (in % divider == 0) {
+          innerPrimeFactors(in/divider, divider, archived:+divider)
+        } else {
+          innerPrimeFactors(in, divider+1, archived)
+        }
+      }
+      innerPrimeFactors(n, 2, List[Int]())
+    }
+  }
+
+  def main(args: Array[String]) = {
+    println(315.primeFactors)
+  }
+}
