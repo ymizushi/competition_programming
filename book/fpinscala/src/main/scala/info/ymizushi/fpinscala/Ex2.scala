3aa9b225c5cbb845746cd71e5813859807bd5fe8
ymizushi <mizushi@gmail.com>
2017-12-03 10:20:18 +0900

    solve fpinscala Ex2.4

--- book/fpinscala/src/main/scala/info/ymizushi/fpinscala/Ex2.scala
+++ book/fpinscala/src/main/scala/info/ymizushi/fpinscala/Ex2.scala
@@ -1,37 +1,26 @@
 object Ex2 {
   def main(args: Array[String]) = {
-    // Exercise 2.1
     println(fib(19))
     println(fib_b(19))
 
-    // Exercise 2.2
     println(isSorted(Array(1, 2, 3, 4, 6) , (a: Int ,b: Int) => a < b ))
     println(isSorted(Array(2, 1, 3, 4, 6) , (a: Int ,b: Int) => a < b ))
 
-    // Exercise 2.3
     val f = (x:Int, y:String) => x.toString + y 
-    val hoge = curry(f)
+    val curriedF = curry(f)
     println(f)
     println(f(1, "hoge"))
-    println(hoge(1)("hoge"))
+    println(curriedF(1)("hoge"))
+
+    val curryF = (x:Int) => (y:String) => x.toString + y 
+    val uncurriedF = uncurry(curryF)
+    println(curryF)
+    println(curryF(1)("hoge"))
+    println(uncurriedF(1, "hoge"))
+
   }
 
-  def curry[A, B, C](f: (A, B) => C): A => B => C = 
-    (a:A) => (b:B) => f(a, b)
-
-  @annotation.tailrec
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-    val head = as.head
-    val rest = as.tail
-    if (rest.isEmpty) 
-      true
-    else if (ordered(head, rest.head))
-      isSorted(rest, ordered)
-    else
-      false
-  }
-  
-
+  // Exercise 2.1
   def fib(tn: Int): Int = {
     @annotation.tailrec
     def fib_n(n_2: Int, n_1:Int, n: Int):Int = {
@@ -59,5 +48,26 @@ object Ex2 {
     }
   }
 
+
+  // Exercise 2.2
+  @annotation.tailrec
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    val head = as.head
+    val rest = as.tail
+    if (rest.isEmpty) 
+      true
+    else if (ordered(head, rest.head))
+      isSorted(rest, ordered)
+    else
+      false
+  }
+  
+  // Exercise 2.3
+  def curry[A, B, C](f: (A, B) => C): A => B => C = 
+    (a:A) => (b:B) => f(a, b)
+
+  // Exercise 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = 
+    (a: A, b: B) => f(a)(b)
 }
 
