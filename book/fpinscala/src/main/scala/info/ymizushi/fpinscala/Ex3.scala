6e859b70334ca1827c506509a8e2e1c8852c47b2
ymizushi <mizushi@gmail.com>
2018-02-07 02:41:12 +0900

    solve Ex3.1

---
+++ book/fpinscala/src/main/scala/info/ymizushi/fpinscala/Ex3.scala
@@ -0,0 +1,36 @@
+object Ex3 {
+  sealed trait List[+A]
+  case object Nil extends List[Nothing]
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def sum(ints: List[Int]): Int = ints match {
+      case Nil => 0
+      case Cons(x, xs) => x + sum(xs)
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+      case Nil => 1.0
+      case Cons(0.0, _) => 0.0
+      case Cons(x, xs) => x * product(xs)
+    }
+
+    def apply[A](as: A*): List[A] = 
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+  }
+
+  def main(args: Array[String]) = {
+    // EXERCISE3.1
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x // 実行されない
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y // 3
+      case Cons(h, t) => h + List.sum(t) // 上がマッチしているので実行されない
+      case _ => 101 
+    } // 3
+    println(x)
+  }
+
+
+}
