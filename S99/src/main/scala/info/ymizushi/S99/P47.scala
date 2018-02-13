b4f6c8659a85cc74c296e49899a602c5ad1e8ff8
ymizushi <mizushi@gmail.com>
2018-02-13 20:33:54 +0900

    solve P47

--- S99/src/main/scala/info/ymizushi/S99/P47.scala
+++ S99/src/main/scala/info/ymizushi/S99/P47.scala
@@ -1,13 +1,18 @@
-object P46 {
+object P47 {
 
-  def and(a: Boolean, b: Boolean): Boolean =
-    a && b
+  implicit class AddBooleanToLogical(v: Boolean) {
+    def and(b: Boolean): Boolean = 
+      v && b
 
-  def or(a: Boolean, b: Boolean): Boolean =
-    a || b
+    def or(b: Boolean): Boolean = 
+      v || b
+
+  }
+
+  def not(b: Boolean): Boolean = !b
 
   def main(args: Array[String]) = {
-    table2((a: Boolean, b: Boolean) => and(a, or(a, b)))
+    table2((a: Boolean, b: Boolean) => a and (a or not(b)))
   }
 
   def table2(f: Function2[Boolean, Boolean, Boolean]): Unit = { 
