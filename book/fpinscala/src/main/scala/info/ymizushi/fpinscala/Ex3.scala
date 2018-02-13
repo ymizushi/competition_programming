61714921e22064e777fc4bff491cb8c1edeb1fb2
ymizushi <mizushi@gmail.com>
2018-02-13 22:36:24 +0900

    solve Ex3.2, Ex3.3, Ex3.4, Ex3.5

--- book/fpinscala/src/main/scala/info/ymizushi/fpinscala/Ex3.scala
+++ book/fpinscala/src/main/scala/info/ymizushi/fpinscala/Ex3.scala
@@ -1,9 +1,11 @@
 object Ex3 {
   sealed trait List[+A]
   case object Nil extends List[Nothing]
-  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+  case class Cons[+A](head: A, tail: List[A]) extends List[A] {
+  }
 
   object List {
+    
     def sum(ints: List[Int]): Int = ints match {
       case Nil => 0
       case Cons(x, xs) => x + sum(xs)
@@ -18,6 +20,39 @@ object Ex3 {
     def apply[A](as: A*): List[A] = 
       if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
+
+
+    def tail[A](l: List[A]): List[A] =
+      l match {
+        case Cons(head, tail) => tail
+        case Nil => Nil
+      }
+
+    def setHead[A](l: List[A], r: A): List[A] =
+      l match {
+        case Cons(head, tail) => Cons(r, tail)
+        case Nil => Nil
+      }
+
+    def drop[A](l: List[A], n: Int): List[A] = {
+      (n, l) match {
+        case (_, Nil) => Nil
+        case (0, _) => l
+        case (1, Cons(head, tail)) => tail
+        case (_, Cons(head, tail)) => drop(tail, n-1)
+      }
+    }
+
+
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+      def innerDropWhile(l: List[A], f: A => Boolean, original: List[A]): List[A] = {
+        l match {
+          case Nil => original
+          case Cons(head, tail) => if (f(head)) tail else innerDropWhile(tail, f, original)
+        }
+      }
+      innerDropWhile(l, f, l)
+    }
   }
 
   def main(args: Array[String]) = {
@@ -30,7 +65,26 @@ object Ex3 {
       case _ => 101 
     } // 3
     println(x)
+
+    // EXERCISE 3.2
+    println(List.tail(List(1, 2, 3, 4)))
+    println(List.tail(Nil))
+
+    // EXERCISE 3.3
+    println(List.setHead(List(1, 2, 3, 4), 3))
+    println(List.setHead(Nil, 5))
+
+    // EXERCISE 3.4
+    println(List.drop(Nil, 1))
+    println(List.drop(List(1, 2, 3, 4), 1))
+    println(List.drop(List(1, 2, 3, 4), 2))
+    println(List.drop(List(1, 2, 3, 4), 3))
+    println(List.drop(List(1, 2, 3, 4), 4))
+
+    // EXERCISE 3.5
+    println(List.dropWhile(List(1, 2, 3, 3), (n:Int) =>  n == 1))
+    println(List.dropWhile(List(1, 2, 3, 3), (n:Int) =>  n == 2))
+    println(List.dropWhile(List(1, 2, 3, 3, 4, 4, 3), (n:Int) =>  n == 4))
+    println(List.dropWhile(List(1, 2, 3, 3, 4, 4, 3), (n:Int) =>  n == 10))
   }
-
-
 }
