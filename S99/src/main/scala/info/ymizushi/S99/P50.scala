2d3155a03adcf435f23bdc4b257fb865389bab2e
ymizushi <mizushi@gmail.com>
2018-02-13 21:26:26 +0900

    add P50

---
+++ S99/src/main/scala/info/ymizushi/S99/P50.scala
@@ -0,0 +1,10 @@
+object P50 {
+  def main(args: Array[String]): Unit = {
+    huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5)))
+  }
+
+  def huffman(l: List[(String, Int)]): List[(String, Int)] = {
+    List.empty[(String,Int)]
+    // TODO: 実装する
+  }
+}
