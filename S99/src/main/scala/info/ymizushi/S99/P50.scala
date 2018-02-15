60ca09684c8b5ccc85d831e5bf315b2eb7e5003f
ymizushi <mizushi@gmail.com>
2018-02-16 00:47:02 +0900

    add P50

--- S99/src/main/scala/info/ymizushi/S99/P50.scala
+++ S99/src/main/scala/info/ymizushi/S99/P50.scala
@@ -1,10 +1,22 @@
 object P50 {
   def main(args: Array[String]): Unit = {
-    huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5)))
+    println(huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5))))
   }
 
-  def huffman(l: List[(String, Int)]): List[(String, Int)] = {
-    List.empty[(String,Int)]
-    // TODO: 実装する
+  def huffman(l: List[(String, Int)]): List[(String, String)] = {
+    val sorted = l.sortBy(_._2).reverse
+    val length = sorted.length
+    val result = sorted.zipWithIndex.map {
+      case (s: (String, Int), i: Int) => {
+        if (i == 0) {
+          (s._1, "0")
+        } else if (length == i+1) {
+          (s._1, "1" * i + "1")
+        
+        } else {
+          (s._1, "1" * i + "0")
+        }
+      }
+    }
   }
 }
