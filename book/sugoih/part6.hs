77f4d5d7d7b1858dfddd281ee1b2a59da11febcc
ymizushi <mizushi@gmail.com>
2018-04-20 00:14:23 +0900

    fix part6.hs

--- book/sugoih/part6.hs
+++ book/sugoih/part6.hs
@@ -2,3 +2,20 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+
+wordCount ::  String -> [(String, Int)]
+wordCount str = (map (\l -> (head l, length l)) (group $ sort (words str)))
+
+f >| g = g . f
+
+main = do
+  print $ numUniques [1, 5, 3, 2, 1, 6, 4, 3, 2, 1]
+  print $ wordCount "piyo hoge piyo hoge"
+  let hoge = (+2) >| (*4)
+  print $ wordCount "piyo hoge piyo hoge"
+  print $ hoge 2
+
+
+
+
