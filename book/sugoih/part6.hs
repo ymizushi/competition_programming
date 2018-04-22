1bd70550c6e64043969868279dc8f84f3a0d2e18
ymizushi <mizushi@gmail.com>
2018-04-23 07:14:32 +0900

    edit part6

--- book/sugoih/part6.hs
+++ book/sugoih/part6.hs
@@ -7,15 +7,24 @@ numUniques = length . nub
 wordCount ::  String -> [(String, Int)]
 wordCount str = (map (\l -> (head l, length l)) (group $ sort (words str)))
 
+tails' :: [a] -> [[a]]
+tails' [] = [[]]
+tails' (head : tail) = (head : tail) : (tails' tail)
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
 f >| g = g . f
 
+import Data.Char
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
 main = do
-  print $ numUniques [1, 5, 3, 2, 1, 6, 4, 3, 2, 1]
-  print $ wordCount "piyo hoge piyo hoge"
-  let hoge = (+2) >| (*4)
-  print $ wordCount "piyo hoge piyo hoge"
-  print $ hoge 2
-
-
-
-
+  --- print $ numUniques [1, 5, 3, 2, 1, 6, 4, 3, 2, 1]
+  --- print $ wordCount "piyo hoge piyo hoge"
+  --- let hoge = (+2) >| (*4)
+  --- print $ wordCount "piyo hoge piyo hoge"
+  --- print $ hoge 2
+  print $ tails' ""
+  print $ tails' "abcde"
