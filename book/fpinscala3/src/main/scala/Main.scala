3a4355e4aadc472d4d3f07098dc067b3be74d071
ymizushi <mizushi@gmail.com>
2021-12-18 01:01:15 +0900

    solve Exercise 2.2

--- book/fpinscala3/src/main/scala/Main.scala
+++ book/fpinscala3/src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 @main def hello: Unit = 
   println("hoge")
   println(fib(10))
+  println(isSorted(Array(2, 4, 3, 5), (l, r) => l < r))
 
 def fib(n: Int): Int = {
   if (n==0) {
@@ -23,4 +24,25 @@ def fib_toc(n: Int): Int = {
     }
   }
   internal_fib(n, 0, 1)
+}
+
+def reduceFunc[A](l: A, r: A, f: (A, A) => Boolean): A = {
+  if (f(l, r)) {
+    r
+  } else {
+    l
+  }
+}
+
+def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+  def loop(head: A, tail: Array[A]): Boolean = {
+    if (tail.length==0) {
+      true
+    } else if (ordered(head, tail.head)) {
+      loop(tail.head, tail.tail)
+    } else {
+      false
+    }
+  }
+  loop(as.head, as.tail)
 }
